11af33b571a05723824bf66e1bd832586fc6d4f3
Demmí Elizabeth <119278324+callmedems@users.noreply.github.com>
2026-03-17 11:34:44 -0600

    Class 3, PR 3, challenge 3

---
+++ sql_challenges/challenge-03/README.md
@@ -0,0 +1,4 @@
+# SQL Lesson 10-11
+About aggregate functions
+
+# Tutorial from FreeSQL Aggregating Rows: Databases for Developers
--- sql_challenges/challenge-03/solution.sql
+++ sql_challenges/challenge-03/solution.sql
@@ -24,3 +24,22 @@ GROUP BY Role;
 --      Find the total number of years employed by all Engineers
 SELECT Role, SUM(Years_employed) FROM employees
 WHERE Role = "Engineer";
+
+-- TRY ITS from tutorial
+-- FIRST (punto 4)
+--  Complete the following query to return the: Number of different shapes & 
+--The standard deviation (stddev) of the unique weights
+
+select count (distinct shape),  stddev(distinct weight) from bricks;
+
+-- SECOND (punto 6)
+--  Complete the following query to return the total weight for each shape stored in the bricks table
+select shape, SUM(weight) from bricks
+group by shape;
+
+-- THIRD (punto 8)
+-- Complete the following query to find the shapes which have a total weight less than four
+select shape, sum(weight)
+from BRICKS
+group by shape
+having sum(weight) < 4;
